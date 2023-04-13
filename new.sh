2d10391e6249811ad962ffe6a66c0a018e9aeb82
RRSSMM12 <129393445+RRSSMM12@users.noreply.github.com>
2023-04-13 09:14:04 +0530

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,233 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $pu"          ########   ######  ##     ##     "
+echo "          ##     ## ##    ## ###   ###          "
+echo "          ##     ## ##       #### ####    "
+echo "          ########   ######  ## ### ##"
+echo "          ##   ##         ## ##     ##  "
+echo "          ##    ##  ##    ## ##     ##  "
+echo "          ##     ##  ######  ##     ## "$me" RANI ISE BACK "
+#echo " "
+#echo $i" "
+echo $pu"===============================================  "
+echo "[+] Author        : RSM PROGRAMING   "
+echo "[+] Github        : RRSSMM12    "
+echo "[+] OWNE.         : RANI "
+echo "[+] Status        : FREE   "
+echo "[+] version       : 0.1"
+echo "[+] Tools  Name   : RANDOM "
+echo "==============================================="
+#echo $i"[+] "$bi"YOUTUBE CONTACT "$i": "$me"RANI TRICKER "
+#echo $i"[+] "$bi"MY FRD YT NAME  "$i": "$me"SD TRICKER"
+#echo $i"[+] "$bi"FACEBOOK CONTACT"$i": "$pur"mistyxd0.2 "
+#echo $i""$bi"●"$me"══━═━═━═━━═══━═━═━═"$bi"❴"$pu"MISTY"$bi"❵"$me"═━═━═══━═━═━═━═━═━═"$bi"●   "
+#echo $i"────────────────────────────────    ─────"
+echo " [1] RANDOM INDIAN MENU      "
+#echo $i"|"$ku" 2"$i" |"$ku" TERMUX SET-UPS    "
+#echo $i"|"$ku" 4"$i" |"$ku"RIAZ BYP4SS "
+echo $ku" [A]"$ku" EXIT    "
+echo $pu "==============================================="
+
+echo
+echo $me"┌==="$bi"["$i"R"$bi"]"$me"===="$bi"["$i""S""$bi"]"$me"===="$bi"["$i""M""$bi"]"
+echo $me"¦"
+read -p"└──➣➢➣ " pil
+
+
+#fi
+
+
+
+
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ranikumari12121/raNii
+cd raNii
+python MISTY.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ranikumari12121/raNii
+cd raNii
+python MISTY.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = A ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"TERMUX USE KR NE KE LIYE THANKS 👍"
+sleep 2
+echo $i""
+sleep 2
+echo $ku"FACEBOOK :"$i" https://www.facebook.com/mistyxd0.2"
+echo $ku"Facebook :"$i" RANI KUMARI"
+sleep 2
+echo $pur">> THANK MY TOOL  USE  🧨💝🧚✨🔥<<"
+exit
+fi
